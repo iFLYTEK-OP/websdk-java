83ce7e6ca1bd61ce52cea4ec7f1046dad1d32baf
rblu2 <rblu2@iflytek.com>
2025-02-25 10:42:56 +0800

    星火知识库 1.上传文档用于训练 2.查询文档训练状态 3.星火问答 4.萃取QA对

--- websdk-java-spark/src/test/java/SparkDocTest.java
+++ websdk-java-spark/src/test/java/SparkDocTest.java
@@ -15,7 +15,7 @@ public class SparkDocTest {
     //上传文档，投喂知识
     @Test
     public void test1() {
-        RestResult data = SparkDoc.prepare("6057995a", "YjRkOTBlODAxM2U2NzIyZmMzMDhmMTdk")
+        RestResult data = SparkDoc.prepare("xx", "xx")
                 .file("D:\\draft\\250224\\背影.txt")
                 .upload();
         print(data.getBody());
@@ -24,8 +24,8 @@ public class SparkDocTest {
     //上传文档，投喂知识,查询文档训练状态,fieldId值，在test1上传文档后的返回里结果里获取
     @Test
     public void test2() {
-        RestResult data = SparkDoc.prepare("6057995a", "YjRkOTBlODAxM2U2NzIyZmMzMDhmMTdk")
-                .addFileId("de62193c7ce34a83ad68c0a786d9a4b7")
+        RestResult data = SparkDoc.prepare("xx", "xx")
+                .addFileId("xx")
                 .status();
         print(data.getBody());
 
@@ -34,8 +34,8 @@ public class SparkDocTest {
     //对话问答，（训练状态fileStatus为vectored时）
     @Test
     public void test3() throws InterruptedException {
-        SparkDocChat.prepare("6057995a", "YjRkOTBlODAxM2U2NzIyZmMzMDhmMTdk")
-                .addFileId("253fc5ccaa554007be7eec22bfbf1b1b")
+        SparkDocChat.prepare("xx", "xx")
+                .addFileId("xx")
                 .append(UserMessage.crate("本文的中心思想是什么"))
                 .onMessage(this::print)
                 .execute();
@@ -46,8 +46,8 @@ public class SparkDocTest {
     //提交萃取任务，即对训练完成的文档生成QA对
     @Test
     public void test4() {
-        RestResult data = SparkDoc.prepare("6057995a", "YjRkOTBlODAxM2U2NzIyZmMzMDhmMTdk")
-                .extract("253fc5ccaa554007be7eec22bfbf1b1b");
+        RestResult data = SparkDoc.prepare("xx", "xx")
+                .extract("xx");
         print(data.getBody());
 
     }
--- websdk-java-spark/src/test/java/SparkTest.java
+++ websdk-java-spark/src/test/java/SparkTest.java
@@ -16,7 +16,7 @@ public class SparkTest {
     //http非流式调用，适用于简单的回答
     @Test
     public void test1() {
-        String result = HttpSparkChat.prepare(SparkModelEum.LITE, "VDRBRxhysCQvIYxhMZeK:bUydDCeMlAnBSNtaRnoq")
+        String result = HttpSparkChat.prepare(SparkModelEum.LITE, "xx")
                 .append(UserMessage.crate("来一个妹子喜欢听的笑话"))
                 .execute();
         System.out.println("result " + result);
@@ -25,7 +25,7 @@ public class SparkTest {
     //http流式调用
     @Test
     public void test2() {
-        HttpSparkChat.prepare(SparkModelEum.V4_ULTRA, "VDRBRxhysCQvIYxhMZeK:bUydDCeMlAnBSNtaRnoq")
+        HttpSparkChat.prepare(SparkModelEum.V4_ULTRA, "xx")
                 .webSearch()
                 .append(SystemMessage.crate("你是一个新闻工作者")).append(UserMessage.crate("今日3条热点娱乐新闻"))
                 .execute(this::print);
@@ -34,7 +34,7 @@ public class SparkTest {
     //WEBSOCKET调用
     @Test
     public void test3() throws InterruptedException {
-        WsSparkChat.prepare(SparkModelEum.GENERAL_V35, "6057995a", "28bb4b72196174bb290e8d60876a1013", "YjRkOTBlODAxM2U2NzIyZmMzMDhmMTdk")
+        WsSparkChat.prepare(SparkModelEum.GENERAL_V35, "xx", "xx", "xxx")
                 .append(SystemMessage.crate("你现在扮演李白")).append(UserMessage.crate("你喝醉过吗"))
                 .onMessage(this::print)
                 .execute();
